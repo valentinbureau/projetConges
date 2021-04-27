8cecb3d490aee2ae193c95766944ad88ab187c34
valentinbureau <72617401+valentinbureau@users.noreply.github.com>
2021-04-27 18:12:58 +0200

    Delete formationSrpingBootRest directory

--- formationSrpingBootRest/src/main/java/formation/sopra/formationSrpingBoot/controllers/rest/Common.java
+++
@@ -1,5 +0,0 @@
-package formation.sopra.formationSrpingBoot.controllers.rest;
-
-public class Common {
-
-}
--- formationSrpingBootRest/src/main/java/formation/sopra/formationSrpingBoot/repositories/ModuleRepository.java
+++
@@ -1,9 +0,0 @@
-package formation.sopra.formationSrpingBoot.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import formation.sopra.formationSrpingBoot.entities.Module;
-
-public interface ModuleRepository extends JpaRepository<Module, Integer> {
-	
-}
