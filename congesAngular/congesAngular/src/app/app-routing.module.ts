3a1ddef6d30511439c0869ddf6478d1c251f17b4
valentinbureau <valentin-bureau2@outlook.fr>
2021-04-28 15:03:34 +0200

    routing

--- congesAngular/congesAngular/src/app/app-routing.module.ts
+++ congesAngular/congesAngular/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DemandeComponent } from './demande/demande.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //{ path: 'demande/employe/:id', component: },
+  { path: 'demande/employe/:id/edit', component: DemandeComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'home', component: HomeComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- congesAngular/congesAngular/src/app/app.module.ts
+++ congesAngular/congesAngular/src/app/app.module.ts
@@ -5,22 +5,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DemandeComponent } from './demande/demande.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { LoginComponent } from './auth/login/login.component';
+import { SharedModule } from './shared/shared-module';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemandeComponent,
     LoginComponent,
+    HomeComponent,
 
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-   ReactiveFormsModule,
+    ReactiveFormsModule,
     SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
