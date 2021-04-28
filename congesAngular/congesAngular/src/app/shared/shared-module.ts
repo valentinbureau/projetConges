b5ff41c0c45886c58979121793b11dc939682834
valentinbureau <valentin-bureau2@outlook.fr>
2021-04-28 10:35:59 +0200

    update SharedModule

--- congesAngular/congesAngular/src/app/shared/shared-module.ts
+++ congesAngular/congesAngular/src/app/shared/shared-module.ts
@@ -3,12 +3,15 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
 
 const ANGULAR_MATERIAL = [
   MatButtonModule,
   MatInputModule,
   MatIconModule,
   MatCardModule,
+  MatFormField,
+  MatLabel
 ];
 
 @NgModule({
