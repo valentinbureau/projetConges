3d4a9496299227f226bd93b41aa1e85d11fd671e
Maguy <mag.le.glatin@gmail.com>
2021-04-28 17:55:27 +0200

    up

--- congesAngular/congesAngular/src/app/Services/conges.service.ts
+++ congesAngular/congesAngular/src/app/Services/conges.service.ts
@@ -5,33 +5,28 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { EnumCongé } from '../model/enum-congé.enum';
 
-
-
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ServiceNameService {
-  constructor(private httpClient: HttpClient) { }
-
+  constructor(private httpClient: HttpClient) {}
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CongesService {
   private static URL = 'http://127.0.0.1:8080/vacances/api/conge';
-  private httpHeaders : HttpHeaders;
+  private httpHeaders: HttpHeaders;
   keys = Object.keys;
   statut = EnumStatus;
 
-  constructor(private http: HttpClient,
-    )
-    {
-    this.initHeader();}
-
+  constructor(private http: HttpClient) {
+    this.initHeader();
+  }
 
   private initHeader() {
     this.httpHeaders = new HttpHeaders({
       'Content-Type': 'application/json',
-     // Authorization: `Basic ${localStorage.getItem('auth')},
+      // Authorization: `Basic ${localStorage.getItem('auth')},
     });
   }
 
@@ -58,26 +53,22 @@ export class CongesService {
 
   public update(conge: Conge): Observable<Conge> {
     this.initHeader();
-    return this.http.put<Conge>(
-      CongesService.URL + '/' + conge.id,
-      conge,
-      {
-        headers: this.httpHeaders,
-      }
-    );
+    return this.http.put<Conge>(CongesService.URL + '/' + conge.id, conge, {
+      headers: this.httpHeaders,
+    });
   }
 
   public insert(conge: Conge): Observable<Conge> {
     this.initHeader();
     const congeFormate = {
-      typeConge : conge.type,
-      dateDebut : conge.dateDebut,
-      dateFin : conge.dateFin,
-      motif : conge.motif,
-      dateDemande : conge.dateDemande,
-      statut : EnumStatus.EN_COURS_DE_TRAITEMENT,
+      typeConge: conge.type,
+      dateDebut: conge.dateDebut,
+      dateFin: conge.dateFin,
+      motif: conge.motif,
+      dateDemande: conge.dateDemande,
+      statut: EnumStatus.EN_COURS_DE_TRAITEMENT,
     };
-    console.log(conge.dateDebut);
+    //console.log(conge.dateDebut);
     return this.http.post<Conge>(CongesService.URL, congeFormate, {
       headers: this.httpHeaders,
     });
--- congesAngular/congesAngular/src/app/demande/demande.component.ts
+++ congesAngular/congesAngular/src/app/demande/demande.component.ts
@@ -1,36 +1,45 @@
-
 import { CongesService } from './../Services/conges.service';
 import { Conge } from './../model/conge';
 import { Component, OnInit, NgModule } from '@angular/core';
 import { EnumCongé } from '../model/enum-congé.enum';
 import { ActivatedRoute, Router } from '@angular/router';
-
-
+import { DateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-demande',
   templateUrl: './demande.component.html',
-  styleUrls: ['./demande.component.css']
+  styleUrls: ['./demande.component.css'],
 })
 export class DemandeComponent implements OnInit {
-conge : Conge = new Conge();
-public dateNow=Date.now();
-keys = Object.keys;
-typeConges = EnumCongé;
-
+  conge: Conge = new Conge();
+  public dateNow = Date.now();
+  keys = Object.keys;
+  typeConges = EnumCongé;
 
   constructor(
-    private activatedRoute : ActivatedRoute,
-    private CongesService : CongesService,
-    private router : Router, ) {  };
-
-  ngOnInit(): void {
+    private activatedRoute: ActivatedRoute,
+    private CongesService: CongesService,
+    private router: Router,
+    private dateAdapter: DateAdapter<Date>
+  ) {
+    this.dateAdapter.setLocale('en-GB'); //dd/MM/yyyy
   }
+
+  ngOnInit(): void {}
 
   save() {
-      this.CongesService.insert(this.conge).subscribe((data) => {
-        this.router.navigate(['/conge']);
-      });
-    }
-  }
+    this.dateAdapter.setLocale('en-GB'); //dd/MM/yyyy
+    console.log('Date debut : ' + this.conge.dateDebut.toLocaleDateString());
+    // const dateDebut = new Date(this.conge.dateDebut.toLocaleDateString());
+    const dateDebut = new Date(this.conge.dateDebut);
+    const dateFin = new Date(this.conge.dateFin.toLocaleDateString());
+    console.log('apres conv : ' + dateDebut);
+    //this.conge.dateDebut = dateDebut;
+    // this.conge.dateFin = dateFin;
+    // this.conge.dateDemande = dateDemande;
 
+    this.CongesService.insert(this.conge).subscribe((data) => {
+      this.router.navigate(['/conge']);
+    });
+  }
+}
